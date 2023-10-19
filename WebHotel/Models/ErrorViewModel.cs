fe66087c98a49088866571c934f25342fc77b0c6
FAT PC <FAT PC@DESKTOP-9ENTE6O>
2023-10-19 20:03:42 +0700

    Add project files.

---
+++ WebHotel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebHotel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
